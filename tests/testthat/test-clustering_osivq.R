c48299a1f1bb5e6b973ef333c47ef85da0fde3b9
Maël Delem <mael.a.delem@gmail.com>
2025-09-17 14:43:43 +0200

    Removing unused coda.plot wrapper to fix tests

--- tests/testthat/test-clustering_osivq.R
+++ tests/testthat/test-clustering_osivq.R
@@ -3,12 +3,9 @@ test_that("The clustering suite works as expected", {
   clustering <- cluster_osivq(df)
   df2 <- df |> add_named_clusters(clustering)
   df3 <- summarise_clustering(df2)
-  p <- plot_osivq_ternary(df2, plot_it = TRUE) |> suppressMessages()
-
 
   expect_contains(class(df), c("tbl", "data.frame"))
   expect_contains(class(df2), c("tbl", "data.frame"))
   expect_contains(class(df3), c("tbl", "data.frame"))
   expect_equal(class(clustering), "list")
-  expect_contains(class(p), c("gg", "ggplot"))
 })
